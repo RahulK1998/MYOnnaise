787be89df80ddbca57cb010c73814d4334cbb9b6
Maestorme <maestorme@gmail.com>
2017-10-07 16:27:48 -0500

    Added if statements

--- game/routes/index.js
+++ game/routes/index.js
@@ -11,34 +11,71 @@ router.get('/', function(req, res, next) {
 Myo.connect('com.stolksdorf.myAwesomeApp', require('ws'));
 
 Myo.on('connected', function(){
-  console.log('connected!', Myo.myos.id);
+  console.log('connected!', this.name);
   Myo.setLockingPolicy('none');
+  //console.log(Myo.myos[0].name);
 });
 
 
 Myo.on('fist', function(){
-	console.log('Fist');
-	this.vibrate();
+	if(this.name == "My Myo"){
+		console.log('Fist 1!');
+		this.vibrate();
+	}
+	
+	if(this.name == "Evan 2"){
+		console.log('Fist 2!');
+		this.vibrate();
+	}
 });
 
 Myo.on('wave_in', function(){
-	console.log('Wave In!');
-	this.vibrate();
+	if(this.name == "My Myo"){
+		console.log('Wave In 1!');
+		this.vibrate();
+	}
+	
+	if(this.name == "Evan 2"){
+		console.log('Wave In 2!');
+		this.vibrate();
+	}
+	
 });
 
 Myo.on('wave_out', function(){
-	console.log('Wave Out!');
-	this.vibrate();
+	if(this.name == "My Myo"){
+		console.log('Wave Out 1!');
+		this.vibrate();
+	}
+	
+	if(this.name == "Evan 2"){
+		console.log('Wave Out 2!');
+		this.vibrate();
+	}
 });
 
 Myo.on('fingers_spread', function(){
-	console.log('Fingers Spread!');
-	this.vibrate();
+	if(this.name == "My Myo"){
+		console.log('Fingers Spread 1!');
+		this.vibrate();
+	}
+	
+	if(this.name == "Evan 2"){
+		console.log('Fingers Spread 2!');
+		this.vibrate();
+	}
 });
 
 Myo.on('double_tap', function(){
-	console.log('Double Tap!');
-	this.vibrate();
+	if(this.name == "My Myo"){
+		console.log('Double Tap 1!');
+		this.vibrate();
+	}
+	
+	if(this.name == "Evan 2"){
+		console.log('Double Tap 2!');
+		this.vibrate();
+	}
 });
 
 
